c623236764803526e364ae7ee60bbfe1c837633a
Mateusz Szczap <mateusz.szczap@gmail.com>
2014-01-12 23:04:02 +0100

    inext ver

--- app/adstream/adstream.go
+++ app/adstream/adstream.go
@@ -2,21 +2,14 @@ package adstream
 
 import (
 	"fmt"
-	"code.google.com/p/go.net/websocket"
 	"encoding/base64"
-	"encoding/xml"
+	"code.google.com/p/go.net/websocket"
 	"github.com/mati1979/go-revel-mobile-cars-adstream/app/xmlcodec"
 )
 
-type AdEvent struct {
-	XMLName xml.Name `xml:"http://services.mobile.de/schema/search event"`
-	typ string `xml:"http://services.mobile.de/schema/search event-type"`
-	ad Ad `xml:"http://services.mobile.de/schema/ad ad"`
-}
-
-type Ad struct {
-	XMLName xml.Name `xml:"http://services.mobile.de/schema/ad ad"`
-	key string `xml:"ad-key,attr"`
+type AdData struct {
+	AdId string
+	ZipCode string
 }
 
 func Connect() {
@@ -27,7 +20,7 @@ func Connect() {
 		fmt.Println(fmt.Sprintf("error1:%s", err))
 		return
 	}
-	login := "xxx" + ":" + "xxx"
+	login := "xxx" + ":" + "xx"
 	auth := "Basic " + base64.StdEncoding.EncodeToString([]byte(login))
 	config.Header.Add("Authorization", auth)
 
@@ -38,15 +31,17 @@ func Connect() {
 	}
 
 	for {
-		var event AdEvent
+		var event xmlcodec.AdEvent
 		err := xmlcodec.XMLCodec.Receive(ws, &event)
 		if err != nil {
 			fmt.Println(fmt.Sprintf("error4:%s", err))
 			return
 		}
-		fmt.Println(fmt.Sprintf("event:%s", event))
+		if event.EventType == "AD_CREATE_OR_UPDATE" {
+			adData := AdData{event.Ad.AdKey, event.Ad.Seller.SellerAddress.SellerZipCode.ZipCode}
+			fmt.Println(fmt.Sprintf("ad-data:%s", adData))
+		}
 	}
-
 }
 
 func Init() {
--- app/xmlcodec/xmlcodec.go
+++ app/xmlcodec/xmlcodec.go
@@ -1,10 +1,43 @@
 package xmlcodec
 
 import (
-"encoding/xml"
-"code.google.com/p/go.net/websocket"
-//"fmt"
+	"encoding/xml"
+	"code.google.com/p/go.net/websocket"
 )
+
+type AdEvent struct {
+	XMLName xml.Name `xml:"http://services.mobile.de/schema/search event,omitempty"`
+	EventType string `xml:"event-type,omitempty"`
+	Ad *Ad `xml:"ad,omitempty"`
+}
+
+type Ad struct {
+	XMLName xml.Name `xml:"http://services.mobile.de/schema/ad ad,omitempty"`
+	AdKey string `xml:"key,attr,omitempty"`
+	Seller *Seller `xml:"seller,omitempty"`
+}
+
+type Seller struct {
+	XMLName xml.Name `xml:"http://services.mobile.de/schema/seller seller,omitempty"`
+	SellerKey string `xml:"key,attr,omitempty"`
+	SellerAddress *SellerAddress `xml:"address,omitempty"`
+}
+
+type SellerAddress struct {
+	XMLName xml.Name `xml:"http://services.mobile.de/schema/seller address,omitempty"`
+	SellerZipCode *SellerZipCode `xml:"zipcode,omitempty"`
+	SellerCountryCode *SellerCountryCode `xml:"country-code,omitempty"`
+}
+
+type SellerZipCode struct {
+	XMLName xml.Name `xml:"http://services.mobile.de/schema/seller zipcode,omitempty"`
+	ZipCode string `xml:"value,attr,omitempty"`
+}
+
+type SellerCountryCode struct {
+	XMLName xml.Name `xml:"http://services.mobile.de/schema/seller country-code,omitempty"`
+	CountryCode string `xml:"value,attr,omitempty"`
+}
 
 func xmlMarshal(v interface{}) (msg []byte, payloadType byte, err error) {
 	//buff := &bytes.Buffer{}
